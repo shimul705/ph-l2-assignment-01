7254949381d062e67f16c5024b005bd01d23801f
shimul705 <freelancershimul172@gmail.com>
2025-11-16 19:15:04 +0600

    This is test commit 2

--- Test.ts
+++ Test.ts
@@ -1,3 +1,18 @@
-const test: String = "this is a test"
+class Pizza {
+    topping: string;
+    size: number;
 
-console.log(test);
+
+    constructor(topping: string, size: number) {
+        this.topping = topping;
+        this.size = size;
+    }
+
+    getMyData() {
+        return `Serving a ${this.size} Inch pizza!, With ${this.topping}`
+    }
+}
+
+const pizza = new Pizza("Chicken", 20)
+
+console.log(pizza.getMyData());
