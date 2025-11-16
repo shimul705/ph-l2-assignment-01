770ba4ac7b22895fc5e619e8522a9408ad94d282
shimul705 <freelancershimul172@gmail.com>
2025-11-16 19:29:46 +0600

    This Final Coimmit Before Assigmnent 1

--- Test.ts
+++ Test.ts
@@ -15,4 +15,42 @@ class Pizza {
 
 const pizza = new Pizza("Chicken", 20)
 
-console.log(pizza.getMyData());
+// console.log(pizza.getMyData());
+
+
+
+
+
+// === Encapsulation ===
+
+class Car {
+    brand: string;
+    speed: number;
+
+    // Constructor
+    constructor(brand: string, speed: number) {
+        this.brand = brand;
+        this.speed = speed;
+
+    }
+
+    // Method 
+    drive() {
+        return `This is ${this.brand} & it's speed is ${this.speed} Km/h`
+    }
+
+
+}
+// instans
+const audi = new Car("Audi", 240)
+const toyota = new Car("toyota", 100)
+
+console.log(audi.drive());
+console.log(toyota.drive());
+
+
+// 4 Piller
+// Encapsulation
+// Inheritense
+// abstract
+// Polymorphisom
